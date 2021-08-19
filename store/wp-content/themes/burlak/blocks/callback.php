d2f99fa4fc67d9097843f531961cfdfb94fd7cb5
Eugene Burlak <burlakeugene@gmail.com>
2021-08-19 17:58:54 +0300

    upd

--- store/wp-content/themes/burlak/blocks/callback.php
+++ store/wp-content/themes/burlak/blocks/callback.php
@@ -1,4 +1,21 @@
+<?php
+  $id = 86;
+  $fields = get_fields($id);
+?>
 <div class="modal" id="callback">
-  dsds
-  <!-- <?= do_shortcode('[contact-form-7 id="8" title="Contact form"]') ?> -->
+  <?php if($fields['title']): ?>
+  <div class="modal__title">
+    <?= $fields['title'] ?>
+  </div>
+  <?php endif; ?>
+  <?php if($fields['text']): ?>
+  <div class="modal__text">
+    <?= $fields['text'] ?>
+  </div>
+  <?php endif; ?>
+  <?php if($fields['shortcode']): ?>
+  <div class="modal__text">
+  <?= do_shortcode($fields['shortcode']) ?>
+  </div>
+  <?php endif; ?>
 </div>
